d5a2fcf6451b35e95fd033f80e700d79a1243be2
Bhupesh Sharma <bhupesh.sharma@linaro.org>
2023-06-06 20:37:03 +0530

    arch/arm/mach-snapdragon/include/mach/sysmap-qrb5165rb5.h: merge
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma@linaro.org>

--- arch/arm/mach-snapdragon/include/mach/sysmap-qrb5165rb5.h
+++ arch/arm/mach-snapdragon/include/mach/sysmap-qrb5165rb5.h
@@ -13,11 +13,40 @@
 #define APCS_CLOCK_BRANCH_ENA_VOTE	(0x79004)
 
 /* Uart clock control registers */
-#define DEBUG_UART_APPS_CBCR		(0x1726C)
-#define DEBUG_UART_APPS_CMD_RCGR	(0x17270)
-#define DEBUG_UART_APPS_CFG_RCGR	(0x17274)
-#define DEBUG_UART_APPS_M		(0x17278)
-#define DEBUG_UART_APPS_N		(0x1727C)
-#define DEBUG_UART_APPS_D		(0x17280)
+#define DEBUG_UART_APPS_CBCR		(0x184CC)
+#define DEBUG_UART_APPS_CMD_RCGR	(0x184D0)
+#define DEBUG_UART_APPS_CFG_RCGR	(0x184D4)
+#define DEBUG_UART_APPS_M		(0x184D8)
+#define DEBUG_UART_APPS_N		(0x184DC)
+#define DEBUG_UART_APPS_D		(0x184E0)
+
+/* USB controller clock control registers */
+#define USB30_SEC_MASTER_CBCR		(0x10010)
+#define USB30_SEC_SLEEP_CBCR		(0x10018)
+#define USB30_SEC_MOCK_UTMI_CBCR	(0x1001C)
+#define CFG_NOC_USB3_SEC_AXI_CBCR	(0x1007C)
+#define AGGRE_USB3_SEC_AXI_CBCR		(0x10080)
+#define USB3_SEC_CLKREF_EN		(0x8C010)
+
+#define USB3_SEC_PHY_AUX_CBCR		(0x10054)
+#define USB3_SEC_PHY_COM_AUX_CBCR	(0x10058)
+#define USB3_SEC_PHY_PIPE_CBCR		(0x1005C)
+#define USB3_SEC_PHY_PIPE_MUXR		(0x10060)
+
+#define USB30_SEC_MASTER_CMD_RCGR	(0x10020)
+#define USB30_SEC_MASTER_CFG_RCGR	(0x10024)
+#define USB30_SEC_MASTER_M		(0x10028)
+#define USB30_SEC_MASTER_N		(0x1002C)
+#define USB30_SEC_MASTER_D		(0x10030)
+
+/* SDCC2 controller clock control registers */
+#define SDCC2_BCR		(0x14000)
+#define SDCC2_APPS_CBCR		(0x14004)
+#define SDCC2_AHB_CBCR		(0x14008)
+#define SDCC2_CMD_RCGR		(0x1400C)
+#define SDCC2_CFG_RCGR		(0x14010)
+#define SDCC2_M			(0x14014)
+#define SDCC2_N			(0x14018)
+#define SDCC2_D			(0x1401c)
 
 #endif
