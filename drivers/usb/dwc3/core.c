b40541c83f4b0cb9c8bab78c4d077f63aa84db9e
Bhupesh Sharma <bhupesh.sharma@linaro.org>
2023-08-23 12:49:53 +0530

    FIXME: needed?? drivers/usb/dwc3: Add support for quirks and diff revs (remove debug logs later)
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma@linaro.org>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -257,6 +257,7 @@ static int dwc3_alloc_event_buffers(struct dwc3 *dwc, unsigned length)
 	int			i;
 
 	num = DWC3_NUM_INT(dwc->hwparams.hwparams1);
+	printf("%s, Requesting event buffer count %d\n", __func__, num);
 	dwc->num_event_buffers = num;
 
 	dwc->ev_buffs = memalign(CONFIG_SYS_CACHELINE_SIZE,
@@ -418,6 +419,7 @@ static void dwc3_cache_hwparams(struct dwc3 *dwc)
 {
 	struct dwc3_hwparams	*parms = &dwc->hwparams;
 
+	printf("%s, Entering func..\n", __func__);
 	parms->hwparams0 = dwc3_readl(dwc->regs, DWC3_GHWPARAMS0);
 	parms->hwparams1 = dwc3_readl(dwc->regs, DWC3_GHWPARAMS1);
 	parms->hwparams2 = dwc3_readl(dwc->regs, DWC3_GHWPARAMS2);
@@ -457,6 +459,16 @@ static void dwc3_hsphy_mode_setup(struct dwc3 *dwc)
 	dwc3_writel(dwc->regs, DWC3_GUSB2PHYCFG(0), reg);
 }
 
+/* Global HWPARAMS0 Register */
+#define DWC3_GHWPARAMS0_MODE(n)		((n) & 0x3)
+
+#define DWC3_GUSB3PIPECTL_UX_EXIT_PX	BIT(27)
+
+/* Global HWPARAMS0 Register */
+#define DWC3_GHWPARAMS0_MODE(n)		((n) & 0x3)
+#define DWC3_GHWPARAMS0_MODE_GADGET	0
+#define DWC3_GHWPARAMS0_MODE_HOST	1
+#define DWC3_GHWPARAMS0_MODE_DRD	2
 /**
  * dwc3_phy_setup - Configure USB PHY Interface of DWC3 Core
  * @dwc: Pointer to our controller context structure
@@ -468,6 +480,12 @@ static void dwc3_phy_setup(struct dwc3 *dwc)
 	reg = dwc3_readl(dwc->regs, DWC3_GUSB3PIPECTL(0));
 
 	/*
+	 * Make sure UX_EXIT_PX is cleared as that causes issues with some
+	 * PHYs. Also, this bit is not supposed to be used in normal operation.
+	 */
+	reg &= ~DWC3_GUSB3PIPECTL_UX_EXIT_PX;
+
+	/*
 	 * Above 1.94a, it is recommended to set DWC3_GUSB3PIPECTL_SUSPHY
 	 * to '0' during coreConsultant configuration. So default value
 	 * will be '0' when the core is reset. Application needs to set it
@@ -475,6 +493,14 @@ static void dwc3_phy_setup(struct dwc3 *dwc)
 	 */
 	if (dwc->revision > DWC3_REVISION_194A)
 		reg |= DWC3_GUSB3PIPECTL_SUSPHY;
+
+	/*
+	 * For DRD controllers, GUSB3PIPECTL.SUSPENDENABLE must be cleared after
+	 * power-on reset, and it can be set after core initialization, which is
+	 * after device soft-reset during initialization.
+	 */
+	if (dwc->dr_mode == USB_DR_MODE_OTG)
+		reg &= ~DWC3_GUSB3PIPECTL_SUSPHY;
 
 	if (dwc->u2ss_inp3_quirk)
 		reg |= DWC3_GUSB3PIPECTL_U2SSINP3OK;
@@ -522,6 +548,8 @@ static void dwc3_phy_setup(struct dwc3 *dwc)
 
 	if (dwc->dis_u2_susphy_quirk)
 		reg &= ~DWC3_GUSB2PHYCFG_SUSPHY;
+	else
+		reg |= DWC3_GUSB2PHYCFG_ENBLSLPM;
 
 	if (dwc->dis_enblslpm_quirk)
 		reg &= ~DWC3_GUSB2PHYCFG_ENBLSLPM;
@@ -581,6 +609,17 @@ static void dwc3_set_incr_burst_type(struct dwc3 *dwc)
 	dwc3_writel(dwc->regs, DWC3_GSBUSCFG0, cfg);
 }
 
+#define DWC3_GSNPS_ID(p)	(((p) & DWC3_GSNPSID_MASK) >> 16)
+#define DWC3_IP_IS(_ip)							\
+	(dwc->ip == _ip##_IP)
+#define DWC3_IP			0x5533
+#define DWC31_IP		0x3331
+#define DWC32_IP		0x3332
+
+
+#define DWC3_VER_NUMBER		0xc1a0
+#define DWC3_VER_TYPE		0xc1a4
+
 /**
  * dwc3_core_init - Low-level initialization of DWC3 Core
  * @dwc: Pointer to our controller context structure
@@ -595,6 +634,7 @@ static int dwc3_core_init(struct dwc3 *dwc)
 	int			ret;
 
 	reg = dwc3_readl(dwc->regs, DWC3_GSNPSID);
+#if 0
 	/* This should read as U3 followed by revision number */
 	if ((reg & DWC3_GSNPSID_MASK) != 0x55330000) {
 		dev_err(dwc->dev, "this is not a DesignWare USB3 DRD Core\n");
@@ -602,6 +642,24 @@ static int dwc3_core_init(struct dwc3 *dwc)
 		goto err0;
 	}
 	dwc->revision = reg;
+#else
+	dwc->ip = DWC3_GSNPS_ID(reg);
+
+	/* This should read as U3 followed by revision number */
+	if (DWC3_IP_IS(DWC3)) {
+		dwc->revision = reg;
+	} else if (DWC3_IP_IS(DWC31) || DWC3_IP_IS(DWC32)) {
+		dwc->revision = dwc3_readl(dwc->regs, DWC3_VER_NUMBER);
+		dwc->version_type = dwc3_readl(dwc->regs, DWC3_VER_TYPE);
+	} else {
+		dev_err(dwc->dev, "this is not a DesignWare USB3 DRD Core\n");
+		ret = -ENODEV;
+		goto err0;
+	}
+
+	printf("%s, DWC3 revision: 0x%x\n", __func__, dwc->revision);
+#endif
+
 
 	/* Handle USB2.0-only core configuration */
 	if (DWC3_GHWPARAMS3_SSPHY_IFC(dwc->hwparams.hwparams3) ==
@@ -1138,32 +1196,66 @@ void dwc3_of_parse(struct dwc3 *dwc)
 	}
 }
 
+#define DWC3_VER_IS_PRIOR(_ip, _ver)					\
+	(DWC3_IP_IS(_ip) && dwc->revision < _ip##_REVISION_##_ver)
+
+#define DWC3_VER_IS_WITHIN(_ip, _from, _to)				\
+	(DWC3_IP_IS(_ip) &&						\
+	 dwc->revision >= _ip##_REVISION_##_from &&			\
+	 (!(_ip##_REVISION_##_to) ||					\
+	  dwc->revision <= _ip##_REVISION_##_to))
+
+/* Global User Control 1 Register */
+#define DWC3_GUCTL1_DEV_DECOUPLE_L1L2_EVT	BIT(31)
+#define DWC3_GUCTL1_TX_IPGAP_LINECHECK_DIS	BIT(28)
+#define DWC3_GUCTL1_DEV_FORCE_20_CLK_FOR_30_CLK	BIT(26)
+#define DWC3_GUCTL1_DEV_L1_EXIT_BY_HW		BIT(24)
+#define DWC3_GUCTL1_PARKMODE_DISABLE_SS		BIT(17)
+#define DWC3_GUCTL1_PARKMODE_DISABLE_HS		BIT(16)
+#define DWC3_GUCTL1_RESUME_OPMODE_HS_HOST	BIT(10)
+
+#define DWC3_REVISION_270A	0x5533270a
+#define DWC3_REVISION_280A	0x5533280a
+#define DWC3_REVISION_290A	0x5533290a
+#define DWC3_REVISION_300A	0x5533300a
+#define DWC3_REVISION_310A	0x5533310a
+#define DWC3_REVISION_330A	0x5533330a
+
+#define DWC3_REVISION_ANY	0x0
+#define DWC31_REVISION_ANY	0x0
+
 int dwc3_init(struct dwc3 *dwc)
 {
 	int ret;
 	u32 reg;
 
+	printf("%s, Entering func..\n", __func__);
 	dwc3_cache_hwparams(dwc);
 
+	printf("%s, Calling dwc3_alloc_event_buffers..\n", __func__);
 	ret = dwc3_alloc_event_buffers(dwc, DWC3_EVENT_BUFFERS_SIZE);
 	if (ret) {
 		dev_err(dwc->dev, "failed to allocate event buffers\n");
 		return -ENOMEM;
 	}
 
+	printf("%s, Calling dwc3_core_init..\n", __func__);
 	ret = dwc3_core_init(dwc);
 	if (ret) {
 		dev_err(dwc->dev, "failed to initialize core\n");
 		goto core_fail;
 	}
 
+	printf("%s, Calling dwc3_event_buffers_setup..\n", __func__);
 	ret = dwc3_event_buffers_setup(dwc);
 	if (ret) {
 		dev_err(dwc->dev, "failed to setup event buffers\n");
 		goto event_fail;
 	}
 
+	printf("%s, Checking DWC3 Rev: 0x%x..\n", __func__, dwc->revision);
 	if (dwc->revision >= DWC3_REVISION_250A) {
+		printf("%s, DWC3 Rev: 0x%x\n", __func__, dwc->revision);
 		reg = dwc3_readl(dwc->regs, DWC3_GUCTL1);
 
 		/*
@@ -1173,12 +1265,27 @@ int dwc3_init(struct dwc3 *dwc)
 		if (dwc->revision >= DWC3_REVISION_290A)
 			reg |= DWC3_GUCTL1_DEV_L1_EXIT_BY_HW;
 
+		/*
+		 * Decouple USB 2.0 L1 & L2 events which will allow for
+		 * gadget driver to only receive U3/L2 suspend & wakeup
+		 * events and prevent the more frequent L1 LPM transitions
+		 * from interrupting the driver.
+		 */
+		if (!DWC3_VER_IS_PRIOR(DWC3, 300A))
+			reg |= DWC3_GUCTL1_DEV_DECOUPLE_L1L2_EVT;
+
 		if (dwc->dis_tx_ipgap_linecheck_quirk)
 			reg |= DWC3_GUCTL1_TX_IPGAP_LINECHECK_DIS;
+
+		if (DWC3_VER_IS_WITHIN(DWC3, 290A, ANY) &&
+		    (dwc->maximum_speed == USB_SPEED_HIGH ||
+		     dwc->maximum_speed == USB_SPEED_FULL))
+			reg |= DWC3_GUCTL1_DEV_FORCE_20_CLK_FOR_30_CLK;
 
 		dwc3_writel(dwc->regs, DWC3_GUCTL1, reg);
 	}
 
+	printf("%s, Setting DWC3_GUCTL_HSTINAUTORETRY..\n", __func__);
 	if (dwc->dr_mode == USB_DR_MODE_HOST ||
 	    dwc->dr_mode == USB_DR_MODE_OTG) {
 		reg = dwc3_readl(dwc->regs, DWC3_GUCTL);
@@ -1188,10 +1295,12 @@ int dwc3_init(struct dwc3 *dwc)
 		dwc3_writel(dwc->regs, DWC3_GUCTL, reg);
 	}
 
+	printf("%s, Calling dwc3_core_init_mode..\n", __func__);
 	ret = dwc3_core_init_mode(dwc);
 	if (ret)
 		goto mode_fail;
 
+	printf("%s, Exiting func successfully..\n", __func__);
 	return 0;
 
 mode_fail:
